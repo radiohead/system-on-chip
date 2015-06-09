973b685ff4dc141bf668c1542f3bd435eb0f36aa
Elena Bondarenko <crazypunkbaby@gmail.com>
2015-06-09 20:57:13 +0300

    Exercise 5

---
+++ ex5/ex5.sdk/zynq_design/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.d: ../src/platform.c \
+ ../../zynq_design_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../zynq_design_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../zynq_design_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../zynq_design_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../zynq_design_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../zynq_design_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../zynq_design_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../zynq_design_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
